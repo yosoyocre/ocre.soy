7acae6b076e1c1745cbfa6cda605450638582ec0
Edu Poch <edupoch@gmail.com>
2016-09-05 23:00:14 +0200

    Añadimos archivo de configuración del Deployer

---
+++ deploy.php
@@ -0,0 +1,13 @@
+<?php
+
+	require 'recipe/common.php';
+
+	server('prod', 'http://188.166.156.92/', 22)
+	    ->user('ocre')
+	    ->forwardAgent()
+	    ->stage('prod')
+	    ->env('branch', 'master')
+	    ->env('deploy_path', '/var/www/creoqueocre.es/www');
+
+	set('repository', 'git@github.com:yosoyocre/creoqueocre.es.git');
+?>
