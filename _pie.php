b68ad537c1635488530025f066bd98cca9651f6f
Edu Poch <edupoch@gmail.com>
2016-09-06 00:45:05 +0200

    Facilito el email

--- _pie.php
+++ _pie.php
@@ -4,6 +4,7 @@
   <footer class="navbar bd-navbar" role="banner">
     <nav class="nav navbar-nav">
       <div class="pull-right">
+        <a class="nav-item nav-link" href="mailto:yosoyocre@gmail.com"><i class="fa fa-envelope"></i></a>
         <a class="nav-item nav-link" href="https://www.facebook.com/yosoyocre"><i class="fa fa-facebook"></i></a>
         <a class="nav-item nav-link" href="https://twitter.com/yosoyocre"><i class="fa fa-twitter"></i></a>
         <a class="nav-item nav-link" href="/distante"><span class="separador">/</span> distante</a>
