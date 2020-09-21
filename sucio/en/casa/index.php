de2f9beb79180ccab7178d5f2edd78b002df5365
edupoch <edupoch@gmail.com>
2020-09-21 20:30:37 +0200

    Primeras canciones de la maqueta

---
+++ sucio/en/casa/index.php
@@ -0,0 +1,33 @@
+<?php require('../../../_cabecera.php'); ?>
+
+<div class="row">
+  <div class="col-lg-8">
+
+  <h1>
+      <span class="separador">/</span> <a href="/sucio">sucio</a>
+      <span class="separador">/</span> <a href="/sucio/en">en</a>
+      <span class="separador">/</span> <a href="/sucio/en/casa">casa</a>
+    </h1>
+
+    <p>
+      Queridas y queridos: estoy trabajando en la maqueta del Sucio y esto es lo que tengo por ahora. 
+    </p>
+    <p>
+      Pulsad en los enlaces de aquí abajo para escuchar o descargar los temas. <br>Creo que se escuchan mejor con cascos y con bastante volumen.
+    </p>
+    <p>
+      A ver qué os parecen.
+    </p>    
+
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-lg-6">
+    <p><a href="audio/devenir_maquina_200921.wav">Devenir máquina</a></p>
+    <p><a href="audio/martes_terribles_200921.wav">Martes terribles</a></p>
+  </div>
+  
+</div>
+
+<?php require('../../../_pie.php'); ?>
