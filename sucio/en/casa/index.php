b0640bddfda7116894fa36a9136489434222c526
edupoch <edupoch@gmail.com>
2020-09-27 13:51:36 +0200

    Añadimos la maqueta de Santoalla

--- sucio/en/casa/index.php
+++ sucio/en/casa/index.php
@@ -26,6 +26,7 @@
   <div class="col-lg-6">
     <p><a href="audio/devenir_maquina_200921.mp3">Devenir máquina</a></p>
     <p><a href="audio/martes_terribles_200921.mp3">Martes terribles</a></p>
+    <p><a href="audio/santoalla_20092721.mp3">Santoalla, el musical</a></p>
   </div>
   
 </div>
