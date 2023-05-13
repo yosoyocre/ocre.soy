2a603beedf0a6c7e6d106d626740b535cb53d319
edupoch <edupoch@gmail.com>
2023-05-13 12:50:04 +0200

    Preparamos la configuración de servidor y de Github

---
+++ server_deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+echo "Deploy de la aplicación..."
+
+# Entramos en modo mantenimiento
+(docker exec ocre-app php artisan down) || true
+    # Actualizamos el código
+    git fetch origin master
+    git reset --hard origin/master
+
+    # Copiamos el contenido de dev_docker-compose.yml a docker-compose.yml
+    # para no tener que indicar todo el rato qué fichero usar
+    cp -f server_docker-compose.yml docker-compose.yml
+
+    # Instalamos dependencias basadas en el archivo lock
+    docker exec ocre-app composer install --no-interaction --prefer-dist
+
+    # Forzamos el reinicio de php-fpm
+    # https://stackoverflow.com/questions/37806188/how-to-restart-php-fpm-inside-a-docker-container
+    docker exec ocre-app bash -c "kill -USR2 1"
+
+    chmod +x server_ssl_renew.sh
+
+# Salimos del modo mantenimiento
+docker exec ocre-app php artisan up
+
+echo "Deploy terminado!"
---
+++ server_ssl_renew.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Para hacer que este script se ejecute automáticamente, hay que añadirlo a crontab
+# sudo crontab -e
+# 0 12 * * * /home/ocre/ocre.soy/server_ssl_renew.sh >> /var/log/cron.log 2>&1
+
+COMPOSE="/usr/bin/docker-compose --ansi never"
+DOCKER="/usr/bin/docker"
+
+cd /home/ocre/ocre.soy
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP nginx
+$DOCKER system prune -af
