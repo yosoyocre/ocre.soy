85ac7188d591954a9c74194ca4b618da489c620b
edupoch <edupoch@gmail.com>
2023-05-13 17:25:14 +0200

    Conexión desde Github por ssh key

--- server_ssl_renew.sh
+++ server_ssl_renew.sh
@@ -4,7 +4,7 @@
 # sudo crontab -e
 # 0 12 * * * /home/ocre/ocre.soy/server_ssl_renew.sh >> /var/log/cron.log 2>&1
 
-COMPOSE="/usr/bin/docker-compose --ansi never"
+COMPOSE="/usr/bin/docker compose --ansi never"
 DOCKER="/usr/bin/docker"
 
 cd /home/ocre/ocre.soy
