634aaf100b151d274701730c2d86bc9274fcb011
edupoch <edupoch@gmail.com>
2023-08-08 10:34:54 +0200

    Primeras pruebas para generar varias portadas del débil a la vez

--- debil/en/diseno/multiple/index.php
+++ debil/en/diseno/multiple/index.php
@@ -1,111 +1,106 @@
-<?php 
-  if (isset($_POST['generar'])) :
+<?php
+if (isset($_POST['generar'])) :
 ?>
 
   <html lang="es">
-    <head>
-      <meta charset="UTF-8">
-      <title>Sucio Múltiple</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta http-equiv="x-ua-compatible" content="ie=edge">
-      <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
-      
-      <style>
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Débil Múltiple</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
+
+    <style>
+      .pagina {
+        position: relative;
+        height: 100vh;
+      }
+
+      .portada canvas {
+        width: 12cm !important;
+        height: auto !important;
+      }
+
+      .portada.front canvas {
+        border-right: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+      }
+
+      .portada.back canvas {
+        border-right: 1px solid #ddd;
+        border-top: 1px solid #ddd;
+      }
+
+      @media print {
         .pagina {
-          position: relative;
-          height: 100vh;
-        }
-
-        .portada canvas {
-            width: 12cm !important;
-            height: auto !important;
-        }
-
-        .portada.front canvas {
-            border-right: 1px solid #ddd;
-            border-bottom: 1px solid #ddd;
+          page-break-after: always;
         }
 
         .portada.back canvas {
-            border-right: 1px solid #ddd;
-            border-top: 1px solid #ddd;
+          position: absolute;
+          bottom: 0;
+          left: 0;
         }
+      }
+    </style>
 
-        @media print {
-          .pagina {
-            page-break-after: always;
-          }
+    <?php
+    $dedicatorias = [];
 
-          .portada.back canvas {
-            position: absolute;
-            bottom: 0;
-            left: 0;
-          }
-        }
-      </style>
-      
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-      <script src="/vendor/bootstrap/js/bootstrap.js"></script>
+    if ($_POST['dedicatorias']) {
+      $dedicatorias = preg_split('/\r\n|[\r\n]/', trim($_POST['dedicatorias']));
+    }
+    ?>
 
-      <script src="../js/p5.min.js"></script>
-      <script src="../js/sucio.js"></script>
+  </head>
 
-      <?php
-        $dedicatorias = [];
+  <body>
 
-        if ($_POST['dedicatorias']) {
-          $dedicatorias = preg_split('/\r\n|[\r\n]/', trim($_POST['dedicatorias']));
-        }
-      ?>
-      
-      <script>
-        $(function() {
-          let dedicatorias = <?= json_encode($dedicatorias) ?>;
-          let n = <?= intval($_POST['n']) ?>;
-          let i;          
-          
-          for (i = 0; i < n; i++) {
-            let matiz = Math.round(Math.random() * 255);
-            
-            let paramsPortada = {};
-            paramsPortada.coverType = 'front';
-            paramsPortada.matizGlobal = matiz;            
-            
-            let paramsContra = {};
-            paramsContra.coverType = 'back';
-            paramsContra.matizGlobal = matiz;
-            if (dedicatorias[i]) {
-              paramsContra.textoCreditos = dedicatorias[i];
-            }
+    <div id="disenos"></div>
 
-            let portada = document.createElement('div');
-            portada.className = 'portada front';
-            let contra = document.createElement('div');
-            contra.className = 'portada back';
-            
-            let front = new p5(sucio(paramsPortada), portada);            
-            let back = new p5(sucio(paramsContra), contra);
+    <script type="module">
+      import {
+        crea
+      } from "../js/debil.js";
 
-            let pagina = document.createElement('div');
-            pagina.className = 'pagina';
+      let dedicatorias = <?= json_encode($dedicatorias) ?>;
+      let n = <?= intval($_POST['n']) ?>;
+      let i;
 
-            pagina.appendChild(portada);
-            pagina.appendChild(contra);
+      let disenos = document.getElementById("disenos");
 
-            $('body').append(pagina);
-          }
+      for (i = 0; i < n; i++) {
+
+        let portada = document.createElement('div');
+        portada.className = 'portada front';
+        portada.id = 'portada_' + i;
+
+        let contra = document.createElement('div');
+        contra.className = 'portada back';
+        contra.id = 'contra_' + i;
+
+        let pagina = document.createElement('div');
+        pagina.className = 'pagina';
+
+        pagina.appendChild(portada);
+        pagina.appendChild(contra);
+        disenos.appendChild(pagina);
+
+        crea({
+          portada: "#portada_" + i,
+          contra: "#contra_" + i,
+          conMovimiento: false,
         });
-      </script>
+      }
+    </script>
 
-    </head>
+  </body>
 
-    <body>     
-
-    </body>
   </html>
 
-<?php 
-  else :
+<?php
+else :
 ?>
 
   <?php require('../../../../_cabecera.php'); ?>
@@ -113,14 +108,14 @@
   <div class="row">
     <div class="col-lg-8">
       <h1>
-        <span class="separador">/</span> <a href="/sucio">sucio</a>
-        <span class="separador">/</span> <a href="/sucio/en">en</a>
-        <span class="separador">/</span> <a href="/sucio/en/diseno">diseño</a>
-        <span class="separador">/</span> <a href="/sucio/en/diseno/multiple">multiple</a>
+        <span class="separador">/</span> <a href="/debil">débil</a>
+        <span class="separador">/</span> <a href="/debil/en">en</a>
+        <span class="separador">/</span> <a href="/debil/en/diseno">diseño</a>
+        <span class="separador">/</span> <a href="/debil/en/diseno/multiple">multiple</a>
       </h1>
 
       <p>
-        Este es un sistema para generar varios diseños del <a href="/sucio/en/diseno">sucio</a> a la vez. <br>
+        Este es un sistema para generar varios diseños del <a href="/debil/en/diseno">débil</a> a la vez. <br>
         Para que quede bien, hay que convertirla a <b>CMYK</b> e imprimirla a color <b>mate</b> en un papel de <b>180grs</b>.
       </p>
 
@@ -144,6 +139,6 @@
 
   <?php require('../../../../_pie.php'); ?>
 
-<?php 
-  endif;
+<?php
+endif;
 ?>
