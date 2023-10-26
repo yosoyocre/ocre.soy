62df053247d5efdd7f05d3c8b757c8056d7dc35a
edupoch <edupoch@gmail.com>
2023-10-26 17:28:23 +0200

    Corrija la url de Github

--- _pie.php
+++ _pie.php
@@ -6,7 +6,7 @@ if (!isset($ocultaNav) || !$ocultaNav) :
       <div class="pull-right">
         <a class="nav-item nav-link" href="mailto:yosoyocre@gmail.com"><i class="fa fa-envelope"></i></a>
         <a class="nav-item nav-link" href="https://instagram.com/odepoche"><i class="fa fa-instagram"></i></a>
-        <a class="nav-item nav-link" href="https://github.com/edupoch"><i class="fa fa-github"></i></a>
+        <a class="nav-item nav-link" href="https://github.com/yosoyocre"><i class="fa fa-github"></i></a>
         <a class="nav-item nav-link" href="/distante"><span class="separador">/</span> distante</a>
       </div>
     </nav>
