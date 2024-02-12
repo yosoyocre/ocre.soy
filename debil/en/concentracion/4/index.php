aeb9571988a25b5626c7d6276ae168d8bb2370d5
edupoch <edupoch@gmail.com>
2024-02-12 20:50:17 +0100

    Deformación de caras usando cuadrados

--- debil/en/concentracion/4/index.php
+++ debil/en/concentracion/4/index.php
@@ -35,60 +35,146 @@ colofon([
 <script>
     var folio;
     var img;
+    var tamanoCuadrado = 125;
 
     function preload() {
         img = loadImage('edu_oso_cartel.png');
     }
 
+    function izquierda(x, y) {
+        return [x - tamanoCuadrado, y];
+    }
+
+    function derecha(x, y) {
+        return [x + tamanoCuadrado, y];
+    }
+
+    function arriba(x, y) {
+        return [x, y - tamanoCuadrado];
+    }
+
+    function abajo(x, y) {
+        return [x, y + tamanoCuadrado];
+    }
+
+    // function setup() {
+    //     folio = new Folio();
+
+    //     let tamanoCuadrado = 31;
+
+    //     let inicioX = img.width / 2 - 70;
+    //     let finX = img.width;
+
+    //     let inicioY = 0;
+    //     let finY = img.height;
+
+    //     img.loadPixels();
+
+    //     for (let x = inicioX; x < finX; x += tamanoCuadrado) {
+    //         for (let y = inicioY; y < finY; y += tamanoCuadrado) {
+    //             let mediaRojos = 0;
+    //             let mediaVerdes = 0;
+    //             let mediaAzules = 0;
+
+    //             let cuadrados = 0;
+
+    //             for (let i = 0; i < tamanoCuadrado; i++) {
+    //                 for (let j = 0; j < tamanoCuadrado; j++) {
+    //                     if (x + i >= img.width || y + j >= img.height) {
+    //                         continue;
+    //                     }
+    //                     let pixel = img.get(x + i, y + j);
+    //                     let r = red(pixel);
+    //                     let g = green(pixel);
+    //                     let b = blue(pixel);
+
+    //                     mediaRojos += r;
+    //                     mediaVerdes += g;
+    //                     mediaAzules += b;
+
+    //                     cuadrados++;
+    //                 }
+    //             }
+
+    //             mediaRojos /= cuadrados;
+    //             mediaVerdes /= cuadrados;
+    //             mediaAzules /= cuadrados;
+
+    //             for (let i = 0; i < tamanoCuadrado; i++) {
+    //                 for (let j = 0; j < tamanoCuadrado; j++) {
+    //                     if (x + i >= img.width || y + j >= img.height) {
+    //                         continue;
+    //                     }
+    //                     img.set(x + i, y + j, color(mediaRojos, mediaVerdes, mediaAzules));
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     img.updatePixels();
+
+    //     image(img, 0, 0, folio.width, folio.height);
+    // }
+
     function setup() {
         folio = new Folio();
 
-        let tamanoCuadrado = 31;
-
-        let inicioX = img.width / 2 - 70;
-        let finX = img.width;
-
-        let inicioY = 0;
-        let finY = img.height;
-
         img.loadPixels();
 
-        for (let x = inicioX; x < finX; x += tamanoCuadrado) {
-            for (let y = inicioY; y < finY; y += tamanoCuadrado) {
-                let mediaRojos = 0;
-                let mediaVerdes = 0;
-                let mediaAzules = 0;
+        let inicioX;
+        let inicioY;
 
-                let cuadrados = 0;
+        let inicioDeformacionX = 160;
+        let inicioDeformacionY = 120;
 
+        inicioX = inicioDeformacionX;
+        inicioY = inicioDeformacionY;
+        for (let x = inicioX; x < inicioX + tamanoCuadrado; x += tamanoCuadrado) {
+            for (let y = inicioY; y < inicioY + tamanoCuadrado; y += tamanoCuadrado) {
                 for (let i = 0; i < tamanoCuadrado; i++) {
                     for (let j = 0; j < tamanoCuadrado; j++) {
-                        if (x + i >= img.width || y + j >= img.height) {
-                            continue;
-                        }
-                        let pixel = img.get(x + i, y + j);
-                        let r = red(pixel);
-                        let g = green(pixel);
-                        let b = blue(pixel);
-
-                        mediaRojos += r;
-                        mediaVerdes += g;
-                        mediaAzules += b;
-
-                        cuadrados++;
+                        let t = derecha(x + i, y + j);
+                        t = abajo(t[0], t[1]);
+                        img.set(x + i, y + j, img.get(t[0], t[1]));
                     }
                 }
-
-                mediaRojos /= cuadrados;
-                mediaVerdes /= cuadrados;
-                mediaAzules /= cuadrados;
-
+            }
+        }
+        inicioX = inicioDeformacionX + tamanoCuadrado;
+        inicioY = inicioDeformacionY;
+        for (let x = inicioX; x < inicioX + tamanoCuadrado; x += tamanoCuadrado) {
+            for (let y = inicioY; y < inicioY + tamanoCuadrado; y += tamanoCuadrado) {
                 for (let i = 0; i < tamanoCuadrado; i++) {
                     for (let j = 0; j < tamanoCuadrado; j++) {
-                        if (x + i >= img.width || y + j >= img.height) {
-                            continue;
-                        }
-                        img.set(x + i, y + j, color(mediaRojos, mediaVerdes, mediaAzules));
+                        let t = abajo(x + i, y + j);
+                        t = izquierda(t[0], t[1]);
+                        img.set(x + i, y + j, img.get(t[0], t[1]));
+                    }
+                }
+            }
+        }
+        inicioX = inicioDeformacionX;
+        inicioY = inicioDeformacionY + tamanoCuadrado;
+        for (let x = inicioX; x < inicioX + tamanoCuadrado; x += tamanoCuadrado) {
+            for (let y = inicioY; y < inicioY + tamanoCuadrado; y += tamanoCuadrado) {
+                for (let i = 0; i < tamanoCuadrado; i++) {
+                    for (let j = 0; j < tamanoCuadrado; j++) {
+                        let t = arriba(x + i, y + j);
+                        t = derecha(t[0], t[1]);
+                        img.set(x + i, y + j, img.get(t[0], t[1]));
+                    }
+                }
+            }
+        }
+        inicioX = inicioDeformacionX + tamanoCuadrado;
+        inicioY = inicioDeformacionY + tamanoCuadrado;
+        for (let x = inicioX; x < inicioX + tamanoCuadrado; x += tamanoCuadrado) {
+            for (let y = inicioY; y < inicioY + tamanoCuadrado; y += tamanoCuadrado) {
+                for (let i = 0; i < tamanoCuadrado; i++) {
+                    for (let j = 0; j < tamanoCuadrado; j++) {
+                        let t = arriba(x + i, y + j);
+                        t = izquierda(t[0], t[1]);
+                        img.set(x + i, y + j, img.get(t[0], t[1]));
                     }
                 }
             }
