44215724a5b16d2e8f4b160524f08531b4ab22d2
edupoch <edupoch@gmail.com>
2024-02-13 02:29:09 +0100

    Eliminamos por ahora el cartel pixelado

--- debil/en/concentracion/4/index.php
+++ debil/en/concentracion/4/index.php
@@ -73,65 +73,6 @@ colofon([
         return [floor(x + PI / 2), floor(y + PI / 2)];
     }
 
-    // function setup() {
-    //     folio = new Folio();
-
-    //     let tamanoCuadrado = 31;
-
-    //     let inicioX = img.width / 2 - 70;
-    //     let finX = img.width;
-
-    //     let inicioY = 0;
-    //     let finY = img.height;
-
-    //     img.loadPixels();
-
-    //     for (let x = inicioX; x < finX; x += tamanoCuadrado) {
-    //         for (let y = inicioY; y < finY; y += tamanoCuadrado) {
-    //             let mediaRojos = 0;
-    //             let mediaVerdes = 0;
-    //             let mediaAzules = 0;
-
-    //             let cuadrados = 0;
-
-    //             for (let i = 0; i < tamanoCuadrado; i++) {
-    //                 for (let j = 0; j < tamanoCuadrado; j++) {
-    //                     if (x + i >= img.width || y + j >= img.height) {
-    //                         continue;
-    //                     }
-    //                     let pixel = img.get(x + i, y + j);
-    //                     let r = red(pixel);
-    //                     let g = green(pixel);
-    //                     let b = blue(pixel);
-
-    //                     mediaRojos += r;
-    //                     mediaVerdes += g;
-    //                     mediaAzules += b;
-
-    //                     cuadrados++;
-    //                 }
-    //             }
-
-    //             mediaRojos /= cuadrados;
-    //             mediaVerdes /= cuadrados;
-    //             mediaAzules /= cuadrados;
-
-    //             for (let i = 0; i < tamanoCuadrado; i++) {
-    //                 for (let j = 0; j < tamanoCuadrado; j++) {
-    //                     if (x + i >= img.width || y + j >= img.height) {
-    //                         continue;
-    //                     }
-    //                     img.set(x + i, y + j, color(mediaRojos, mediaVerdes, mediaAzules));
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     img.updatePixels();
-
-    //     image(img, 0, 0, folio.width, folio.height);
-    // }
-
     var escala = 2.5;
     var ladoRombo;
     var anguloPeq;
