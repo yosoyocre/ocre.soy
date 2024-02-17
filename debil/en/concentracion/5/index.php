f89ad4c9a544b4601b6dd89ad2e60927084b0e16
edupoch <edupoch@gmail.com>
2024-02-17 11:21:51 +0100

    Pruebas con puntos sobre líneas

--- debil/en/concentracion/5/index.php
+++ debil/en/concentracion/5/index.php
@@ -44,287 +44,142 @@ colofon([
 
     let v0, v1, v2;
 
+    function yEnLinea(linea, x) {
+        let m = (linea[3] - linea[1]) / (linea[2] - linea[0]);
+        let n = linea[1] - m * linea[0];
+        return m * x + n;
+    }
+
     function setup() {
         folio = new Folio();
 
         let conFondo = true;
         let conContenido = true;
-        let debug = 0;
+        let debug = 1;
 
-        let minLado = 200;
-        let maxLado = 300;
-        let nLineas = 6;
+        let minLado = 50;
+        let maxLado = 50;
+        let nLineas = 3;
+        let nTriangulosPorLinea = 3;
 
         let limiteIzquierdo = folio.width / 4;
         let limiteDerecho = folio.width - limiteIzquierdo;
         let limiteSuperior = folio.height / 4;
         let limiteInferior = folio.height - limiteSuperior;
 
-        // var p = new PoissonDiskSampling({
-        //     shape: [limiteDerecho - limiteIzquierdo, limiteInferior - limiteSuperior],
-        //     minDistance: maxLado / 3,
-        //     maxDistance: maxLado * 2,
-        //     tries: 10
-        // });
-        // var puntos = p.fill();
-
-        // let nTriangulos = puntos.length;
-
-        // let triangulos = [];
-        // for (let i = 1; i <= nTriangulos; i++) {
-        //     let coords = [];
-        //     // En P5, los vectores se crean con base en el origen de coordenadas (0 , 0)
-        //     v0 = createVector(random(minLado, maxLado), 0);
-        //     v1 = p5.Vector.rotate(v0, random(PI / 2, PI / 3));
-
-        //     coords.push(0);
-        //     coords.push(0);
-
-        //     coords.push(v0.x);
-        //     coords.push(v0.y);
-
-        //     coords.push(v1.x);
-        //     coords.push(v1.y);
-
-        //     triangulos.push(coords);
-        // }
-
-        // strokeWeight(5);
-        // stroke(0);
-        // fill(0);
-
-        // if (debug) {
-        //     push();
-        //     tint(255, 255 / 4);
-        //     image(img, 0, 0, folio.width, folio.height);
-        //     pop();
-        // }
-
-        // for (let i = 0; i < triangulos.length; i++) {
-        //     let triangulo = triangulos[i];
-
-        //     let x = puntos[i][0] + limiteIzquierdo;
-        //     let y = puntos[i][1] + limiteSuperior;
-        //     let rotacion = random(0, TWO_PI);
-
-        //     if (conFondo) {
-        //         push();
-        //         translate(x, y);
-        //         rotate(rotacion);
-        //         triangle(
-        //             triangulo[0], triangulo[1],
-        //             triangulo[2], triangulo[3],
-        //             triangulo[4], triangulo[5]
-        //         );
-        //         pop();
-        //     }
-
-        //     if (conContenido) {
-        //         let m = createGraphics(folio.width, folio.height);
-        //         m.translate(x, y);
-        //         m.rotate(rotacion);
-        //         m.fill(0);
-        //         m.triangle(
-        //             triangulo[0], triangulo[1],
-        //             triangulo[2], triangulo[3],
-        //             triangulo[4], triangulo[5]
-        //         );
-        //         let nueva = img.get();
-        //         nueva.mask(m);
-        //         image(nueva, 0, 0, folio.width, folio.height);
-        //     }
-        // }
-
-        // if (debug) {
-        //     // DEBUG límites
-        //     push();
-        //     stroke(0);
-        //     strokeWeight(1);
-        //     line(limiteIzquierdo, 0, limiteIzquierdo, folio.height);
-        //     line(limiteDerecho, 0, limiteDerecho, folio.height);
-        //     line(0, limiteSuperior, folio.width, limiteSuperior);
-        //     line(0, limiteInferior, folio.width, limiteInferior);
-        //     pop();
-
-        //     for (let i = 0; i < triangulos.length; i++) {
-        //         let x = puntos[i][0] + limiteIzquierdo;
-        //         let y = puntos[i][1] + limiteSuperior;
-
-        //         // DEBUG de puntos de origen
-        //         push();
-        //         translate(x, y);
-        //         stroke(0, 255, 0);
-        //         strokeWeight(10);
-        //         point(0, 0);
-        //         pop();
-        //     }
-        // }
-
-        function intersect_point(point1, point2, point3, point4) {
-            const ua = ((point4[0] - point3[0]) * (point1[1] - point3[1]) -
-                    (point4[1] - point3[1]) * (point1[0] - point3[0])) /
-                ((point4[1] - point3[1]) * (point2[0] - point1[0]) -
-                    (point4[0] - point3[0]) * (point2[1] - point1[1]));
-
-            const ub = ((point2[0] - point1[0]) * (point1[1] - point3[1]) -
-                    (point2[1] - point1[1]) * (point1[0] - point3[0])) /
-                ((point4[1] - point3[1]) * (point2[0] - point1[0]) -
-                    (point4[0] - point3[0]) * (point2[1] - point1[1]));
-
-            const x = point1[0] + ua * (point2[0] - point1[0]);
-            const y = point1[1] + ua * (point2[1] - point1[1]);
-
-            return [x, y]
-        }
-
         let lineas = [];
         let intersecciones = [];
+        let triangulos = [];
 
-        for (let i = 0; i < nLineas; i++) {
+        while (lineas.length < nLineas) {
             let x1;
             let y1;
 
             let x2;
             let y2;
             if (random() > 0.5) {
-                x1 = random(0, folio.width);
+                x1 = random(limiteIzquierdo, limiteDerecho);
                 y1 = 0;
 
-                x2 = random(0, folio.width);
+                x2 = random(limiteIzquierdo, limiteDerecho);
                 y2 = folio.height;
             } else {
                 x1 = 0;
-                y1 = random(0, folio.height);
+                y1 = random(limiteSuperior, limiteInferior);
 
                 x2 = folio.width;
-                y2 = random(0, folio.height);
+                y2 = random(limiteSuperior, limiteInferior);
+            }
+
+            let m = (x2 - x1) / (y2 - y1);
+            console.log(m);
+
+            if (abs(m) < 0.1) {
+                continue;
             }
 
             lineas.push([x1, y1, x2, y2]);
-        }
-
-        for (let i = 0; i < lineas.length; i++) {
-            for (let j = i + 1; j < lineas.length; j++) {
-                let interseccion = intersect_point([lineas[i][0], lineas[i][1]], [lineas[i][2], lineas[i][3]], [lineas[j][0], lineas[j][1]], [lineas[j][2], lineas[j][3]]);
-                if (interseccion[0] > 0 && interseccion[0] < folio.width && interseccion[1] > 0 && interseccion[1] < folio.height) {
-                    intersecciones.push(interseccion);
-                }
-            }
-        }
-
-        if (debug) {
-            stroke(255, 255, 0);
-            strokeWeight(5);
-            for (let i = 0; i < lineas.length; i++) {
-                line(lineas[i][0], lineas[i][1], lineas[i][2], lineas[i][3]);
-            }
-        }
-
-        if (debug) {
-            stroke(255, 0, 0);
-            strokeWeight(10);
-            for (let i = 0; i < intersecciones.length; i++) {
-                point(intersecciones[i][0], intersecciones[i][1]);
-            }
-        }
-
-        // Order intersecciones by y
-        intersecciones.sort((a, b) => a[1] - b[1]);
-
-        let triangulos = [];
-
-        for (let i = 0; i < intersecciones.length - 1; i += 2) {
-            let punto1 = intersecciones[i];
-            let punto2 = intersecciones[i + 1];
-
-            let m1 = (punto2[1] - punto1[1]) / (punto2[0] - punto1[0]); //calculate slope of line1
-            let m2 = -1 / (m1) //perpendicular slope is -1/m for line 1
-            let b2 = punto1[1] - punto1[0] * m2 //find b of line2 (y = mx+b) using a new point (x3,y3)
-
-            let nX = punto1[0] + (random(25) + 25) * (random() > 0.5 ? 1 : -1);
-            let nY = m2 * nX + b2;
-
-            triangulos.push([punto1[0], punto1[1], punto2[0], punto2[1], nX, nY]);
 
             if (debug) {
-                triangle(
-                    punto1[0], punto1[1], punto2[0], punto2[1], nX, nY
-                );
+                let i = lineas.length - 1;
+                if (i == 0) {
+                    stroke(255, 0, 0);
+                } else if (i == 1) {
+                    stroke(0, 255, 0);
+                } else if (i == 2) {
+                    stroke(0, 0, 255);
+                }
+                line(x1, y1, x2, y2);
             }
         }
 
-        // var p = new PoissonDiskSampling({
-        //     shape: [limiteDerecho - limiteIzquierdo, limiteInferior - limiteSuperior],
-        //     minDistance: maxLado / 3,
-        //     maxDistance: maxLado * 2,
-        //     tries: 10
-        // });
-        // var puntos = p.fill();
 
-        // if (debug) {
-        //     stroke(0, 255, 0);
-        //     strokeWeight(10);
-        //     for (let i = 0; i < puntos.length; i++) {
-        //         point(puntos[i][0] + limiteIzquierdo, puntos[i][1] + limiteSuperior);
-        //     }
-        // }
+        for (let i = 0; i < lineas.length; i++) {
+            let linea = lineas[i];
 
-        // stroke(0, 0, 255);
-        // strokeWeight(10);
+            let xs = [];
 
-        // let triangulos = [];
-        // let maxIntentos = 10;
-        // let intentos;
+            let nIntentos = 0;
+            let maxIntentos = 10;
+            while (xs.length < nTriangulosPorLinea && nIntentos < maxIntentos) {
+                nIntentos++;
+                let x = random(limiteIzquierdo, limiteDerecho);
+                let y = yEnLinea(linea, x);
 
-        // for (let i = 0; i < puntos.length; i++) {
-        //     let x1 = puntos[i][0] + limiteIzquierdo;
-        //     let y1 = puntos[i][1] + limiteSuperior;
+                if (y < limiteSuperior || y > limiteInferior) {
+                    continue;
+                }
+                xs.push(x);
+            }
 
-        //     let interseccion1 = random(intersecciones);
-        //     let x2 = interseccion1[0];
-        //     let y2 = interseccion1[1];
+            xs.sort((a, b) => a - b);
 
-        //     let interseccion2;
-        //     let x3;
-        //     let y3;
+            console.log(xs);
 
-        //     intentos = 0;
-        //     do {
-        //         interseccion2 = random(intersecciones);
-        //         x3 = interseccion2[0];
-        //         y3 = interseccion2[1];
-        //         intentos++;
-        //     } while (x2 == x3 && y2 == y3 && intentos < maxIntentos);
+            for (let j = 0; j < nTriangulosPorLinea; j++) {
+                let x1 = xs[j];
+                let y1 = yEnLinea(linea, x1);
 
-        //     triangulos.push([x1, y1, x2, y2, x3, y3]);
+                let x2 = x1 + random(minLado, maxLado);
+                let y2 = yEnLinea(linea, x2);
 
-        //     if (debug) {
-        //         triangle(
-        //             x1, y1,
-        //             x2, y2,
-        //             x3, y3
-        //         );
-        //     }
-        // }
+                let v0;
 
-        console.log(triangulos);
+                if (random() > 0.5) {
+                    v0 = createVector(x1, y1);
+                } else {
+                    v0 = createVector(x2, y2);
+                }
+
+                push();
+                translate(v0.x, v0.y);
+                let v1 = createVector(x2 - x1, y2 - y1);
+                v1.rotate(HALF_PI);
+                v0.add(v1);
+                pop();
+
+                let x3 = v0.x;
+                let y3 = v0.y;
+
+                triangulos.push([x1, y1, x2, y2, x3, y3]);
+            }
+        }
 
         stroke(0);
         strokeWeight(5);
-
-        let margenMov = 10;
 
         for (let i = 0; i < triangulos.length; i++) {
             let triangulo = triangulos[i];
             let rotacion = random(0, TWO_PI);
 
-            let x1 = triangulo[0] + random(-margenMov, margenMov);
-            let y1 = triangulo[1] + random(-margenMov, margenMov);
-            let x2 = triangulo[2] + random(-margenMov, margenMov);
-            let y2 = triangulo[3] + random(-margenMov, margenMov);
-            let x3 = triangulo[4] + random(-margenMov, margenMov);
-            let y3 = triangulo[5] + random(-margenMov, margenMov);
+            let x1 = triangulo[0];
+            let y1 = triangulo[1];
+            let x2 = triangulo[2];
+            let y2 = triangulo[3];
+            let x3 = triangulo[4];
+            let y3 = triangulo[5];
 
+            stroke(0);
             push();
             triangle(
                 x1, y1,
@@ -345,6 +200,13 @@ colofon([
             let nueva = img.get();
             nueva.mask(m);
             image(nueva, 0, 0, folio.width, folio.height);
+
+            stroke(255, 0, 0);
+            point(x1, y1);
+            stroke(0, 255, 0);
+            point(x2, y2);
+            stroke(0, 0, 255);
+            point(x3, y3);
         }
     }
 
