4f791b4d7102dffb78feec7a9d44e893cd3f8e06
edupoch <edupoch@gmail.com>
2026-01-24 15:39:02 +0100

    Añadimos sección para explicación. Refactoring

--- jeta/en/diseno/index.php
+++ jeta/en/diseno/index.php
@@ -18,15 +18,15 @@
 
         try {
             const enviaPeticion = () => {
-                const input = document.querySelector('.js-pregunta input');
+                const input = document.querySelector('#pregunta input');
                 const valor = input.value.trim();
                 if (valor.length > 0) {
-                    const pregunta = document.querySelector('.js-pregunta');
+                    const pregunta = document.getElementById('pregunta');
 
                     pregunta.classList.add('hidden');
-                    document.querySelector('.js-cargando').classList.remove('hidden');
+                    document.getElementById('cargando').classList.remove('hidden');
                     setTimeout(() => {
-                        document.querySelector('.js-cargando').classList.remove('opacity-0');
+                        document.getElementById('cargando').classList.remove('opacity-0');
                         setTimeout(() => {
                             // Hacemos una petición a la inteligencia artificial con el problema
                             fetch('peticion.php', {
@@ -40,16 +40,21 @@
                                 }).then(response => response.json())
                                 .then(data => {
                                     console.log(data);
-                                    document.querySelector('.js-cargando').classList.add('hidden');
-                                    document.querySelector('.js-respuesta').classList.remove('hidden');
+                                    document.getElementById('cargando').classList.add('hidden');
+                                    document.getElementById('respuesta').classList.remove('hidden');
                                     new p5(jeta(data.solucion));
+
+                                    document.getElementById('explicacion').classList.remove('hidden');
+                                    setTimeout(() => {
+                                        document.getElementById('explicacion').classList.remove('opacity-0');
+                                    }, 3000);
                                 })
                                 .catch(error => {
                                     console.error('Error:', error);
-                                    document.querySelector('.js-cargando').classList.add('hidden');
-                                    document.querySelector('.js-error').classList.remove('hidden');
+                                    document.getElementById('cargando').classList.add('hidden');
+                                    document.getElementById('error').classList.remove('hidden');
                                     setTimeout(() => {
-                                        document.querySelector('.js-error').classList.remove('opacity-0');
+                                        document.getElementById('error').classList.remove('opacity-0');
                                     }, 100);
                                 });
                         }, 3000);
@@ -61,18 +66,20 @@
             Typekit.load({
                 active: function() {
 
-                    const input = document.querySelector('.js-pregunta input');
+                    const input = document.querySelector('#pregunta input');
                     input.addEventListener('keydown', function(event) {
                         if (event.key === 'Enter') {
                             enviaPeticion();
                         }
                     });
 
-                    const boton = document.querySelector('.js-boton');
+                    const boton = document.querySelector('#pregunta button');
                     boton.addEventListener('click', enviaPeticion);
 
-                    const pregunta = document.querySelector('.js-pregunta');
+                    const pregunta = document.getElementById('pregunta');
                     pregunta.classList.remove('opacity-0');
+
+                    // new p5(jeta());
                 },
             });
         } catch (e) {}
@@ -86,7 +93,7 @@
         </a>
     </div>
 
-    <div class="tarjeta opacity-0 js-pregunta flex pt-72 justify-center min-h-screen text-3xl">
+    <div id="pregunta" class="tarjeta opacity-0 flex pt-72 justify-center min-h-screen text-3xl">
         <div class="max-w-2xl w-full text-center">
             <div class="px-4">
                 <p>
@@ -94,7 +101,7 @@
                 </p>
                 <div class="w-full rounded-full pl-6 pr-3 pt-1 pb-4 border border-slate-300 mt-8 shadow-lg text-left">
                     <input type="text" placeholder="¿Qué es lo que te preocupa hoy en tu trabajo?" maxlength="60" class="w-[calc(100%-3rem)] mt-[9px] text-xl focus:outline-none">
-                    <button class="js-boton float-right mt-1 w-10 h-10 flex items-center justify-center bg-slate-800 rounded-full hover:bg-slate-500 cursor-pointer transition-colors">
+                    <button class="float-right mt-1 w-10 h-10 flex items-center justify-center bg-slate-800 rounded-full hover:bg-slate-500 cursor-pointer transition-colors">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
                             <path class="fill-white" d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z" />
                         </svg>
@@ -104,7 +111,7 @@
             </div>
         </div>
     </div>
-    <div class="tarjeta js-cargando hidden opacity-0 flex pt-72 justify-center min-h-screen text-3xl">
+    <div id="cargando" class="hidden tarjeta opacity-0 flex pt-72 justify-center min-h-screen text-3xl">
         <div class="max-w-2xl w-full text-center">
             <div class="px-4">
                 <p class="animate-pulse">
@@ -113,7 +120,7 @@
             </div>
         </div>
     </div>
-    <div class="tarjeta js-error hidden opacity-0 flex pt-72 justify-center min-h-screen text-3xl">
+    <div id="error" class="hidden tarjeta opacity-0 flex pt-72 justify-center min-h-screen text-3xl">
         <div class="max-w-2xl w-full text-center">
             <div class="px-4">
                 <p class="mb-8">
@@ -125,8 +132,13 @@
             </div>
         </div>
     </div>
-    <div class="tarjeta js-respuesta hidden flex items-center justify-center  min-h-screen">
+    <div id="respuesta" class="hidden tarjeta flex items-center justify-center min-h-screen">
         <main class="js-portada w-[500px] h-[500px]"></main>
+    </div>
+    <div id="explicacion" class="hiddem opacity-0 tarjeta flex justify-center min-h-screen text-2xl">
+        <div class="max-w-2xl w-full text-left">
+            <p>Este es tu diseño del disco "Jeta" de Ocre</p>
+        </div>
     </div>
 </body>
 
